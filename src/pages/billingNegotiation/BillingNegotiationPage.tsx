caadd81cf76c96b75cbd556a02d8045fcd3604a3
Jay <w742794749@gmail.com>
2024-11-25 00:49:27 -0500

    add layouts

--- src/pages/billingNegotiation/BillingNegotiationPage.tsx
+++ src/pages/billingNegotiation/BillingNegotiationPage.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Container, Typography, Box } from '@mui/material';
+
+// Components
 import BillingUpload from './components/BillingUpload';
 import IssueTable from './components/IssueTable';
 import CallButton from './components/CallButton';
@@ -17,11 +20,22 @@ const BillingNegotiationPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <BillingUpload onUpload={handleFileUpload} />
-      <IssueTable issues={issues} />
-      <CallButton onCall={handleCallInitiation} />
-    </div>
+    <Container maxWidth="lg">
+      <Typography variant="h4" component="h1" gutterBottom sx={{ marginTop: 4, marginBottom: 2 }}>
+        Billing Negotiation System
+      </Typography>
+      <Box display="flex" flexDirection="column" gap={3}>
+        <Box>
+          <BillingUpload onUpload={handleFileUpload} />
+        </Box>
+        <Box flexGrow={1}>
+          <IssueTable issues={issues} />
+        </Box>
+        <Box>
+          <CallButton onCall={handleCallInitiation} />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
